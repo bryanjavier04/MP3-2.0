e97a098a673e10b34189d61b15b799aa1dbee975
unknown <carlmercadoaz@gmail.com>
2021-06-10 16:05:36 +0800

    Added sample screen output

---
+++ src/controller/AccessStudentDB.java
@@ -0,0 +1,17 @@
+package controller;
+
+public class AccessStudentDB {
+
+	public static void main(String[] args) {
+		System.out.println("[A]-dd Student");
+		System.out.println("[L]-ist Students");
+		System.out.println("[S]-earch Student");
+		System.out.println("[D]-elete Student");
+		System.out.println("[R]-eport Generator");
+		System.out.println("[P]-urge");		
+		System.out.println("[Q]-uit");
+		System.out.println("");
+		System.out.println("Selection Option");
+	}
+
+}
