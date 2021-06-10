23b4eaef5d3476eec2a5099369c0cffdd608643c
Bryan Javier <201901165#iacademy.edu.ph>
2021-06-10 18:25:53 +0800

    JDBC Functions Added and Add, List, Search Functions

--- src/model/DatabaseFunction.java
+++ src/model/DatabaseFunction.java
@@ -1,6 +1,16 @@
 package model;
 
 public interface DatabaseFunction {
+	String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	String JDBC_CONNECTION_URL = "jdbc:mysql://localhost:3306/students";
+	String JDBC_USERNAME = "root";
+	String JDBC_PASSWORD = "";
+	
+	
+	String INSERT_INTO_STUDENTS_INFO = "INSERT INTO student_info(student_id, lastname, firstname,"
+			+ " course, year, units) VALUES(?,?,?,?,?,?)";
+	String SELECT_FROM_STUDENTS_INFO = "SELECT * FROM students_info";
+	String SELECT_SEARCH_STUDENT = "SELECT * FROM students_info WHERE student_id = ";
 	
 	
 
--- src/model/StudentBean.java
+++ src/model/StudentBean.java
@@ -1,5 +1,190 @@
 package model;
 
-public class StudentBean {
+import java.io.Serializable;
+import java.sql.*;
 
+public class StudentBean implements Serializable, DatabaseFunction{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	public static int totalStudents = 0;
+	public static int csStudents = 0;
+	public static int isStudents = 0;
+	public static int itStudents = 0;
+	
+	private String studentId;
+	private String lastName;
+	private String firstName;
+	private String course; 
+	private int year;
+	private int units;
+	
+	public StudentBean() {
+		studentId = "";
+		lastName = "";
+		firstName = "";
+		course = "";
+		year = 0;
+		units = 0;
+	}
+	
+	public StudentBean(String studentId, String lastName, String firstName, String course
+			,int year, int units) {
+		this.studentId = studentId;
+		this.lastName = lastName;
+		this.firstName = firstName;
+		this.course = course;
+		this.year = year;
+		this.units = units;
+		
+	}
+	
+	public String getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getUnits() {
+		return units;
+	}
+
+	public void setUnits(int units) {
+		this.units = units;
+	}
+
+	//JDBC Operations
+	private Connection getConnection() {
+		Connection connection = null;
+		
+		try {
+			//establish java JDBC Driver
+			Class.forName(DatabaseFunction.JDBC_DRIVER);
+			connection = DriverManager.getConnection(DatabaseFunction.JDBC_CONNECTION_URL, 
+					DatabaseFunction.JDBC_USERNAME, DatabaseFunction.JDBC_PASSWORD);
+			if(connection != null) {
+				System.out.println("Connection is valid!");
+			}else {
+				System.out.println("Connection failed!");
+			}
+		}catch(ClassNotFoundException cnfe){
+			System.err.println("Driver not found! " + cnfe.getMessage());
+		}catch(SQLException sqle) {
+			System.err.println(sqle.getMessage());
+		}
+		
+		return connection;
+	}
+	
+	private boolean insertRecord() {
+		boolean isSuccess = false;
+		
+		try {
+			Connection connection = getConnection();
+			if(connection != null) {
+				PreparedStatement pstmt = connection.prepareStatement(DatabaseFunction.INSERT_INTO_STUDENTS_INFO);
+				pstmt.setString(1, this.studentId);
+				pstmt.setString(2, this.lastName);
+				pstmt.setString(3, this.firstName);
+				pstmt.setString(4, this.course);
+				pstmt.setInt(4, this.year);
+				pstmt.setInt(5, this.units);
+				
+				pstmt.executeUpdate();
+				isSuccess = true;
+			}
+		}catch(SQLException sqle) {
+			System.err.println(sqle.getMessage());
+		}
+		
+		return isSuccess;
+	}
+	
+	private boolean selectRecord() {
+		boolean isSuccess = false;
+		
+		try {
+			Connection connection = getConnection();
+			
+			for(int i = 0; i < StudentBean.totalStudents; ++i) {
+				Statement stmt = connection.createStatement();
+				ResultSet rs = stmt.executeQuery(DatabaseFunction.SELECT_FROM_STUDENTS_INFO);
+				
+				System.out.println("ID: " + rs.getString("studentId"));
+				System.out.println("Lastname: " + rs.getString("lastName"));
+				System.out.println("Firstname: " + rs.getString("firstName"));
+				System.out.println("Course: " + rs.getString("course"));
+				System.out.println("Year: " + rs.getInt("year"));
+				System.out.println("Units: " + rs.getInt("units"));
+			}
+			
+			
+			isSuccess = true;
+			
+			
+		}catch(SQLException sqle) {
+			System.err.println(sqle.getMessage());
+		}
+		
+		return isSuccess;
+	}
+	
+	private boolean searchStudent(String student_id) {
+		boolean isSuccess = false;
+		
+		try {
+			Connection connection = getConnection();
+			Statement stmt = connection.createStatement();
+			ResultSet rs = stmt.executeQuery(DatabaseFunction.SELECT_SEARCH_STUDENT + student_id);
+			
+			System.out.println("ID: " + rs.getString("studentId"));
+			System.out.println("Lastname: " + rs.getString("lastName"));
+			System.out.println("Firstname: " + rs.getString("firstName"));
+			System.out.println("Course: " + rs.getString("course"));
+			System.out.println("Year: " + rs.getInt("year"));
+			System.out.println("Units: " + rs.getInt("units"));
+			
+			isSuccess = true;
+		}catch(SQLException sqle) {
+			System.err.println(sqle.getMessage());
+		}
+		
+		return isSuccess;
+	}
 }
